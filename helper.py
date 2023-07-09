ed26823644b07f2b9f28966bfa50e1a387c7a314
dixitesh <dixitesh@msu.edu>
2023-07-08 22:17:59 -0400

    added the infinite loop for casual price lookup

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import yfinance as yf
-
+from typing import List
+import time
 
 def getRealtimeInfo(stock: str) -> float:
   # Create a ticker object for the stock you want to retrieve data for
@@ -9,10 +10,15 @@ def getRealtimeInfo(stock: str) -> float:
   data = ticker.info
 
   # Access currentPrice from the data
-  current_price = data['currentPrice']
+  try:
+    current_price = data['currentPrice']
+  except KeyError:
+    current_price = 0
 
-  # Print the retrieved information
-  print("Current Price:", current_price)
+  debug = False
+  if debug:
+    # Print the retrieved information
+    print("Current Price:", current_price)
 
   return current_price
 
@@ -27,3 +33,10 @@ def getHistorialMonthlyData(stock: str, months: int):
   # Print the retrieved historical stock price data
   print(data)
 
+
+def startRealTimeLoop(stocks: List[str], duration: int) -> List[float]:
+  while True:
+    for stock in stocks:
+      price = getRealtimeInfo(stock)
+      print("{}:{}".format(stock, price))
+    time.sleep(duration)
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import yfinance as yf
+from helper import *
 
+def main():
+    stocks = ["AAPL", "MSFT"]
+    startRealTimeLoop(stocks, 1)
+
+if __name__ == "__main__":
+    main()
+    
